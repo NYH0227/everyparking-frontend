7fa305ce9fd398b9d5693c1fccb9bd04b1d5cae6
NYH0227 <agent960227@gmail.com>
2022-10-07 23:04:45 +0900

    test

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -8,6 +8,7 @@ import { CBreadcrumb, CBreadcrumbItem } from '@coreui/react'
 const AppBreadcrumb = () => {
   const currentLocation = useLocation().pathname
 
+
   const getRouteName = (pathname, routes) => {
     const currentRoute = routes.find((route) => route.path === pathname)
     return currentRoute ? currentRoute.name : false
